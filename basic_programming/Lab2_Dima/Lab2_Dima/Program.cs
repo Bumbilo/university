d9e22676d68621ea84db33731b74d813a06171f7
Dmytro <dimagolyak@gmail.com>
2020-03-26 21:52:39 +0200

    aaded labs

---
+++ basic_programming/Lab2_Dima/Lab2_Dima/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Lab2_Dima
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read, ReadLine, ReadKey, Write(). CursorLeft, CursorTop
+            Console.WriteLine("Hello!\nwrite your name please");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " could you write age");
+            Console.Write("Age: ");
+            // Read
+            int age = Console.Read();
+            Console.Write(Convert.ToChar(age));
+            //MessageBox.Show(&quot; Выход!!! & quot;, &quot; Внимание & quot;, MessageBoxButtons.OK,     MessageBoxIcon.Information);            
+        }
+    }
+}
+ 
