2261c79674780c7d3087bbc345082f29fff288f5
Dmytro <dimagolyak@gmail.com>
2020-03-26 23:02:36 +0200

    Added Linked List

---
+++ structures_and_algorithms/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new Model.LinkedList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            foreach(var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            list.Remove(3);
+            foreach (var item in list)
+            {
+                Console.Write(item + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
