0a313ed50983119d5d7eda1445dcad31685d74f8
Dmytro <dimagolyak@gmail.com>
2020-03-28 20:16:17 +0200

    add easy stack

---
+++ structures_and_algorithms/Stack/Stack/Model/EasyStack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack.Model
+{
+    public class EasyStack<T>
+    {
+        private List<T> items = new List<T>();
+
+        public int Count => items.Count;
+
+        public void Push(T item)
+        {
+            items.Add(item);
+        }
+
+        public T Pop()
+        {
+            if(Count > 0)
+            {
+                var item = items.LastOrDefault();
+                items.Remove(item);
+                return item;
+            }
+            else
+            {
+                throw new NullReferenceException("Stack is empty");
+            }
+        }
+
+        public T Peek()
+        {
+            if(Count > 0)
+            {
+                return items.LastOrDefault();
+            }
+            else
+            {
+                throw new NullReferenceException("Stack is empty");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Stack with {Count} elements";
+        }
+    }
+}
---
+++ structures_and_algorithms/Stack/Stack/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Stack.Model;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var easyStack = new EasyStack<int>();
+
+            easyStack.Push(1);
+            easyStack.Push(2);
+            easyStack.Push(3);
+            easyStack.Push(4);
+
+            var item = easyStack.Pop();
+            var item2 = easyStack.Peek();
+
+            Console.WriteLine(item);
+            Console.WriteLine(item2);
+        }
+    }
+}
